325ec1717eab1ab5d82c99ff16a953fdabb0ef9f
Marcos Fanton <marcos.fanton@ufsm.br>
2024-11-14 17:06:18 -0300

    figs e tabelas para epibr_24

---
+++ code/epibr_24.R
@@ -0,0 +1,245 @@
+# Load packages #### 
+library(tidyverse) 
+library(here) 
+library(scales)
+library(MetBrewer)
+library(showtext)
+library(sjPlot)
+library(sjmisc)
+library(sjlabelled)
+library(cowplot)
+library(gt)
+
+
+# Load models and datasets ####
+strat_level <- read.csv("data/pns19_stratum-obesity.csv", stringsAsFactors = TRUE) |> 
+  dplyr::mutate(id = row_number())
+
+# Config ####
+#Font
+font_add_google("Raleway", "raleway")
+showtext::showtext_auto()
+showtext::showtext_opts(dpi = 300)
+
+# Colors
+colors <- met.brewer("Peru1", 159)
+
+# Colors - Strata
+strat_colors <- tibble(
+  stratum = strat_level$stratum,
+  color = colors
+)
+
+# Dataframe w/ colors
+strat_level <- strat_level |> 
+  left_join(strat_colors, by = "stratum")
+
+# Fig.1 Scatterplot (sorted by obesity) ####  
+fig1 <- strat_level |> 
+  ggplot(aes(y = mBprob_fit, x = rankprob, color = color)) +
+  geom_hline(yintercept = 0.2159, color = "black", 
+             linetype = "dashed", alpha = 0.7,
+            size = 0.5) +
+  geom_point(size = 1) +
+  geom_errorbar(aes(ymin = mBprob_lwr, ymax = mBprob_upr)) +
+  labs(x = "Ranking de Estratos Sociais",
+        y = "Obesidade Predita (%)") +
+  scale_y_continuous(labels = scales::label_percent(scale = 100, suffix = "")) +
+  scale_color_identity() + 
+  theme_bw() +
+  theme(legend.position = "none",
+        text = element_text(family = "raleway", size = 24),
+        axis.title.x = element_text(face = "bold"),
+        axis.title.y = element_text(face = "bold")
+      )
+
+# Save fig1
+ggsave(
+  filename = "fig/fig1_epibr.png",
+  bg = "white",
+  width = 18,
+  height = 7,
+  dpi = 300,
+  plot = fig1)
+
+# Fig. 2 Scatterplot (top-bottom 10)
+# List fo 10 highest and lowest predicted stratum means
+top10 <- bind_rows(
+    strat_level |> 
+      slice_max(rankprob, n = 10) |> 
+      mutate(type = "top"),  # Top 10
+    strat_level |> 
+      slice_min(rankprob, n = 10) |> 
+      mutate(type = "bottom") # Bottom 10
+  )
+# Plot
+fig2 <- top10 |> 
+  ggplot(aes(y = mBprob_fit, x = rankprob, color = color)) +
+  geom_hline(yintercept = 0.2159, color = "black", 
+             linetype = "dashed", alpha = 0.7,
+            size = 0.5) +
+  geom_point(size = 1) +
+  geom_errorbar(aes(ymin = mBprob_lwr, ymax = mBprob_upr)) +
+  labs(x = "Ranking de Estratos Sociais",
+        y = "Obesidade Predita (%)") +
+  scale_y_continuous(labels = scales::label_percent(scale = 100, suffix = "")) +
+  scale_color_identity() + 
+  theme_bw() +
+  theme(legend.position = "none",
+        text = element_text(family = "raleway", size = 24),
+        axis.title.x = element_text(face = "bold"),
+        axis.title.y = element_text(face = "bold")
+      )
+
+# Save fig2
+ggsave(
+  filename = "fig/fig2_epibr.png",
+  bg = "white",
+  width = 18,
+  height = 7,
+  dpi = 300,
+  plot = fig2)
+
+# Table with Top-Bottom 10
+# Recode
+top10 <- top10 |> 
+  dplyr::mutate(stratum = recode(stratum,
+    "womanblackmidIlowElow" = "MulherPretaIdade(t2)Renda(t1)Educação(t1)",
+    "womanblackoldIlowEmid" = "MulherPretaIdade(t3)Renda(t1)Educação(t2)",
+    "womanbrownmidIlowElow" = "MulherPardaIdade(t2)Renda(t1)Educação(t1)",
+    "womanblackmidIlowEmid" = "MulherPretaIdade(t2)Renda(t1)Educação(t2)",
+    "womanblackoldIlowElow" = "MulherPretaIdade(t3)Renda(t1)Educação(t1)",
+    "womanbrownmidImidElow" = "MulherPardaIdade(t2)Renda(t2)Educação(t1)",
+    "womanblackoldIhighEmid" = "MulherPretaIdade(t3)Renda(t3)Educação(t2)",
+    "womanblackmidImidElow" = "MulherPretaIdade(t2)Renda(t2)Educação(t1)",
+    "womanblackmidImidEhigh" = "MulherPretaIdade(t2)Renda(t2)Educação(t3)",
+    "manblackmidImidEmid" = "HomemPretoIdade(t2)Renda(t2)Educação(t2)",
+    "womanwhiteyoungIhighEhigh" = "MulherBrancaIdade(t1)Renda(t3)Educação(t3)",
+    "manbrownyoungIlowEmid" = "HomemPardoIdade(t1)Renda(t1)Educação(t2)",
+    "manbrownyoungIlowElow" = "HomemPardoIdade(t1)Renda(t1)Educação(t1)",
+    "womanwhiteyoungImidEhigh" = "MulherBrancaIdade(t1)Renda(t2)Educação(t3)",
+    "womanbrownyoungIhighEhigh" = "MulherPardaIdade(t1)Renda(t3)Educação(t3)",
+    "womanwhitemidIhighEhigh" = "MulherBrancaIdade(t2)Renda(t3)Educação(t3)",
+    "manwhiteyoungIhighEhigh" = "HomemBrancoIdade(t1)Renda(t3)Educação(t3)",
+    "manwhiteyoungIlowElow" = "HomemBrancoIdade(t1)Renda(t1)Educação(t1)",
+    "womanbrownyoungIlowEhigh" = "MulherPardaIdade(t1)Renda(t1)Educação(t3)",
+    "manwhiteyoungIlowEhigh" = "HomemBrancaIdade(t1)Renda(t1)Educação(t3)"
+  ),
+              mBprob_lwr = mBprob_lwr*100,
+              mBprob_upr = mBprob_upr*100
+            ) 
+# Table Top 10
+tab_top10 <- top10 |> 
+  filter(type == "top") |> 
+  select(stratum, obesity_mean, mBprob_lwr, mBprob_upr) |> 
+  gt() |> 
+  cols_label(
+    stratum = "Estrato",
+    obesity_mean = "(%)",
+    mBprob_lwr = "IC(95%)",
+    mBprob_upr = ""
+  ) |> 
+  cols_unhide(columns = mBprob_upr) |>
+  cols_merge(
+    columns = c(mBprob_lwr, mBprob_upr),
+    pattern = "({1},<< {2})>>",
+  ) |> 
+  tab_header(
+    title = "Obesidade Predita por Estratos Sociais",
+    subtitle = md("Top 10 Estratos com <u>Maior</u> Prevalência")
+  ) |>  
+  tab_spanner(
+    label = "Obesidade",
+    columns = c(obesity_mean, mBprob_lwr, mBprob_upr)
+  ) |> 
+  fmt_number(
+    columns = c(obesity_mean, mBprob_lwr, mBprob_upr),
+    decimals = 2
+  ) |> 
+  opt_table_font(font = "raleway") |> 
+  opt_stylize(style = 1, color = 'gray') |> 
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_column_labels()
+  ) |> 
+    tab_style(
+      style = cell_text(weight = "bold"),
+      locations = cells_title()
+  ) |> 
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_column_spanners()
+)
+# Save table
+gtsave(tab_top10, "fig/tab_top10.png")
+
+# Table Bottom 10
+tab_bottom10 <- top10 |> 
+  filter(type == "bottom") |> 
+  select(stratum, obesity_mean, mBprob_lwr, mBprob_upr) |> 
+  gt() |> 
+  cols_label(
+    stratum = "Estrato",
+    obesity_mean = "(%)",
+    mBprob_lwr = "IC(95%)",
+    mBprob_upr = ""
+  ) |> 
+  cols_unhide(columns = mBprob_upr) |>
+  cols_merge(
+    columns = c(mBprob_lwr, mBprob_upr),
+    pattern = "({1},<< {2})>>",
+  ) |> 
+  tab_header(
+    title = "Obesidade Predita por Estratos Sociais",
+    subtitle = md("Top 10 Estratos com <u>Menor</u> Prevalência")
+  ) |>  
+  tab_spanner(
+    label = "Obesidade",
+    columns = c(obesity_mean, mBprob_lwr, mBprob_upr)
+  ) |> 
+  fmt_number(
+    columns = c(obesity_mean, mBprob_lwr, mBprob_upr),
+    decimals = 2
+  ) |> 
+  opt_table_font(font = "raleway") |> 
+  opt_stylize(style = 1, color = 'gray') |> 
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_column_labels()
+  ) |> 
+    tab_style(
+      style = cell_text(weight = "bold"),
+      locations = cells_title()
+  ) |> 
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_column_spanners()
+)
+# Save table
+gtsave(tab_bottom10, "fig/tab_bottom10.png")
+
+
+
+# Tables ####
+# Table with models 
+tab_model(mA, mB, 
+  p.style = "stars",
+  string.ci = "IC(95%)",
+pred.labels = c("Intercepto",
+"Gênero (Mulher)",
+"Raça (Preta)",
+"Raça (Pardo)",
+"Idade (Tercil2)",
+"Idade (Tercil3)",
+"Renda (Tercil2)",
+"Renda (Tercil3)",
+"Educação (Tercil2)",
+"Educação (Tercil3)"),
+dv.labels = c("Modelo Vazio", "Modelo Ajustado"),
+string.pred = "Coeficiente",
+CSS = list(css.table = '+font-family: Raleway;'))
+
+
+
+
+
